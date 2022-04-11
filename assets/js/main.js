33a927d204178f793be345e886e217e8710f1fe2
NVHViet99 <hoangviet220699@gmail.com>
2022-04-11 16:10:06 +0700

    grid-layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,11 @@
-const btnSetting = document.querySelector(".btn-setting");
-const tableInfor = document.querySelector(".table-infor");
-const tableData = document.querySelector(".table-data");
-const tableInforLine = document.querySelector(".table-infor__line");
+const btnSetting = document.querySelector(".btn-1");
+const btnSubmmit = document.querySelector(".btn-submit");
 const form = document.querySelector(".form");
-const btnSubmit = document.querySelector(".btn-submit");
 
 btnSetting.addEventListener("click", function () {
-  tableInfor.classList.toggle("display-none");
-  tableInforLine.classList.toggle("display-none");
-  tableData.classList.toggle("display-none");
-  form.classList.toggle("display-none");
+  form.classList.toggle("hidden");
 });
 
-btnSubmit.addEventListener("click", function () {
-  tableInfor.classList.remove("display-none");
-  tableInforLine.classList.remove("display-none");
-  tableData.classList.remove("display-none");
-  form.classList.add("display-none");
+btnSubmmit.addEventListener("click", function () {
+  form.classList.add("hidden");
 });

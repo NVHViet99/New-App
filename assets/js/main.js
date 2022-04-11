09cb9556120a474d0ef90acb7b88640ae3b08034
NVHViet99 <hoangviet220699@gmail.com>
2022-04-11 13:26:50 +0700

    edit table

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,20 @@
 const btnSetting = document.querySelector(".btn-setting");
 const tableInfor = document.querySelector(".table-infor");
 const tableData = document.querySelector(".table-data");
+const tableInforLine = document.querySelector(".table-infor__line");
 const form = document.querySelector(".form");
 const btnSubmit = document.querySelector(".btn-submit");
 
 btnSetting.addEventListener("click", function () {
   tableInfor.classList.toggle("display-none");
+  tableInforLine.classList.toggle("display-none");
   tableData.classList.toggle("display-none");
   form.classList.toggle("display-none");
 });
 
 btnSubmit.addEventListener("click", function () {
   tableInfor.classList.remove("display-none");
+  tableInforLine.classList.remove("display-none");
   tableData.classList.remove("display-none");
   form.classList.add("display-none");
 });
